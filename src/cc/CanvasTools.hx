d63e30a7919f74f0a33b5f511929ab6cd3f61e14
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-08 23:00:34 +0100

    looked like a good idea, but didn't need it in the the end... #WIP

--- src/cc/CanvasTools.hx
+++ src/cc/CanvasTools.hx
@@ -23,6 +23,11 @@ class CanvasTools {
 			height = width;
 		ctx.strokeRect(x - width / 2, y - height / 2, width, height);
 	};
+	// static public function centreFillStrokeRect(ctx:CanvasRenderingContext2D, x:Float, y:Float, width:Float, ?height:Float) {
+	// 	if (height == null)
+	// 		height = width;
+	// 	ctx.strokeRect(x - width / 2, y - height / 2, width, height);
+	// };
 
 	/**
 	 * create a rectangle with center point (x,y)
