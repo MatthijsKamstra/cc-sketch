2d03c66f1b582c1cb0fe129eff70564137050cdb
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-15 13:33:04 +0100

    arc

--- src/cc/util/TextUtil.hx
+++ src/cc/util/TextUtil.hx
@@ -3,27 +3,28 @@ package cc.util;
 import js.html.CanvasRenderingContext2D;
 
 class TextUtil {
-	public function new () {
+	public function new() {
 		// your code
 	}
-	/**
-	 *
-	 * 		// important to have a example text in the canvas, otherwise the measurement don't work
-		// important to have the font loaded
 
-	 * @source	 https://stackoverflow.com/questions/2936112/text-wrap-in-a-canvas-element
-	 *
-	 * @param ctx
-	 * @param text
-	 * @param maxWidth
+	/**
+		*
+		* 		// important to have a example text in the canvas, otherwise the measurement don't work
+				// important to have the font loaded
+
+		* @source	 https://stackoverflow.com/questions/2936112/text-wrap-in-a-canvas-element
+		*
+		* @param ctx
+		* @param text
+		* @param maxWidth
 	 */
-	public static function getLines(ctx:CanvasRenderingContext2D, text:String, maxWidth:Float) : Array<String> {
+	public static function getLines(ctx:CanvasRenderingContext2D, text:String, maxWidth:Float):Array<String> {
 		// trace('$text, $maxWidth');
-		var words : Array<String> = text.split(" ");
-		var lines : Array<String> = [];
+		var words:Array<String> = text.split(" ");
+		var lines:Array<String> = [];
 		var currentLine = words[0];
 
-		for ( i in 1 ... words.length ) {
+		for (i in 1...words.length) {
 			// for (var i = 1; i < words.length; i++) {
 			var word = words[i];
 			var width = ctx.measureText(currentLine + " " + word).width;
@@ -38,35 +39,37 @@ class TextUtil {
 		return lines;
 	}
 
+	/**
+		*
+		* ctx.textAlign = "center";
+		* ctx.textBaseline = "bottom";
+		* ctx.font = '20px Source Code Pro';
+		* TextUtil.drawTextAlongArc(ctx, "Hier moet iets staan dat de moeite waard is", centerX, centerY, radius, angle);
 
-// function drawTextAlongArc(context, str, centerX, centerY, radius, angle){
-//                 context.save();
-//                 context.translate(centerX, centerY);
-//                 context.rotate(-1 * angle / 2);
-//                 context.rotate(-1 * (angle / str.length) / 2);
-//                 for (var n = 0; n < str.length; n++) {
-//                     context.rotate(angle / str.length);
-//                     context.save();
-//                     context.translate(0, -1 * radius);
-//                     var char = str[n];
-//                     context.fillText(char, 0, 0);
-//                     context.restore();
-//                 }
-//                 context.restore();
-// }
+		* @param ctx
+		* @param str
+		* @param centerX
+		* @param centerY
+		* @param radius
+		* @param angle
+	 */
+	static public function drawTextAlongArc(ctx:CanvasRenderingContext2D, str:String, centerX:Float, centerY:Float, radius:Int, angle:Float) {
+		var charArr = str.split('');
 
-// window.onload = function(){
-//   var canvas = document.getElementById("myCanvas");
-//   var context = canvas.getContext("2d");
+		ctx.save();
+		ctx.translate(centerX, centerY);
+		ctx.rotate(-1 * angle / 2);
+		ctx.rotate(-1 * (angle / charArr.length) / 2);
 
-//   context.font = "30pt Arial";
-//   context.textAlign = "center";
-
-//   var centerX = canvas.width / 2;
-//   var centerY = canvas.height - 30;
-//   var angle = Math.PI * 0.8; // radians
-//   var radius = 180;
-//   drawTextAlongArc(context, "Zak Ruvalcaba", centerX, centerY, radius, angle);
-// };
-
+		// for (var n = 0; n < str.length; n++) {
+		for (i in 0...charArr.length) {
+			var _char = charArr[i];
+			ctx.rotate(angle / charArr.length);
+			ctx.save();
+			ctx.translate(0, -1 * radius);
+			ctx.fillText(_char, 0, 0);
+			ctx.restore();
+		}
+		ctx.restore();
+	}
 }
