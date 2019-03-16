bef67bf5b27e92477252d6cdb5c463bf3d6ab77e
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-16 14:56:11 +0100

    shuffle array

--- src/cc/util/MathUtil.hx
+++ src/cc/util/MathUtil.hx
@@ -221,6 +221,31 @@ class MathUtil {
 	}
 
 	/**
+	 * Randomly shuffle an array
+	 * https://stackoverflow.com/a/2450976/1293256
+	 * @param  {Array} array The array to shuffle
+	 * @return {String}      The first item in the shuffled array
+	 */
+	static public function shuffle(array:Array<Dynamic>):Array<Dynamic> {
+		var currentIndex = array.length;
+		var temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 != currentIndex) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	};
+
+	/**
 	 * sent a value, and check if it is in the correct range
 	 *
 	 * @example
