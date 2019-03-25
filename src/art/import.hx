99164bac103fcac8e00d90b2f0f83d2a1755371d
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-25 20:37:16 +0100

    update

--- src/art/import.hx
+++ src/art/import.hx
@@ -1,15 +1,12 @@
 // quick and dirty import for Haxe JavaScript
 import js.Browser.*;
 import js.html.*;
-
 // easy access to Globals
 import Sketch.Global.*;
-
 // easy access to the utils static funciton
 import cc.util.MathUtil.*;
 import cc.util.ColorUtil.*;
 import cc.util.AnimateUtil.*;
-
 // utils
 import cc.util.*;
 import cc.util.ColorUtil;
@@ -18,7 +15,6 @@ import cc.util.MathUtil;
 import cc.util.TextUtil;
 // easy access to typedef
 import cc.AST;
-
 import cc.draw.Text;
 
 // syntactic sugar to extend CanvasRenderingContext2D
@@ -28,6 +24,15 @@ using cc.util.ShapeUtil;
 // animation lib
 import cc.lets.Go;
 import cc.lets.easing.*;
-
 // settings
 import quicksettings.QuickSettings;
+import js.Browser.*;
+import js.html.*;
+import Sketch;
+import Sketch.*;
+import cc.model.constants.App;
+import cc.tool.Export;
+import js.html.*;
+import js.Browser.document;
+import js.Browser.window;
+import js.Browser.*;
