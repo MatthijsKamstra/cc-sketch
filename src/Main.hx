9fa368101c484d3274c41d481e8e1f05b9d18dd2
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-25 20:38:30 +0100

    update main for easier navigation

--- src/Main.hx
+++ src/Main.hx
@@ -3,20 +3,71 @@ package;
 import js.Browser.*;
 import js.Browser;
 import js.html.*;
-
 import cc.model.constants.App;
-
 import art.*;
 
-class Main {
+using StringTools;
 
-	public function new () {
-		trace('START :: main');
-		console.log('${App.NAME} :: build: ${App.BUILD} ');
-		var cc = new CC100();
+class Main {
+	var count:Int;
+	var hash:String;
+	var ccTypeArray:Array<Class<Dynamic>> = [CC100, ExportTest, ZipTest, CCLoader];
+
+	public function new() {
+		document.addEventListener("DOMContentLoaded", function(event) {
+			// DOM ready
+			console.log('${App.NAME} :: build: ${App.BUILD}');
+			setupCC();
+			setupNav();
+		});
 	}
 
-	static public function main () {
-		var app = new Main ();
+	function setupCC() {
+		// setup canvas
+		var ctx:CanvasRenderingContext2D = Sketch.create("creative_code_mck");
+
+		// get hash from url
+		hash = js.Browser.location.hash;
+		hash = hash.replace('#', '');
+
+		var clazz = Type.resolveClass('art.${hash}');
+		if (clazz == null) {
+			// make sure if it's not in the list, show the latest Sketch
+			clazz = ccTypeArray[ccTypeArray.length - 1];
+		}
+		count = ccTypeArray.indexOf(clazz);
+		var cc = Type.createInstance(clazz, [ctx]);
+		changeHash();
+	}
+
+	function setupNav() {
+		// make sure the browser updates after changing the hash
+		window.addEventListener("hashchange", function() {
+			location.reload();
+		}, false);
+
+		// use cursor key lef and right to switch sketches
+		window.addEventListener('keydown', function(e:js.html.KeyboardEvent) {
+			switch (e.key) {
+				case 'ArrowRight':
+					count++;
+				case 'ArrowLeft':
+					count--;
+				case 'ArrowUp':
+					count = ccTypeArray.length - 1;
+				case 'ArrowDown':
+					count = 0;
+					// default : trace ("case '"+e.key+"': trace ('"+e.key+"');");
+			}
+			changeHash();
+		}, false);
+	}
+
+	function changeHash() {
+		location.hash = Type.getClassName(ccTypeArray[count]).replace('art.', '');
+	}
+
+	static public function main() {
+		var app = new Main();
 	}
 }
