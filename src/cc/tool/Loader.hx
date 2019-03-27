bd2fb291c7ebab504fea5581bc8f4940452be406
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-27 23:58:28 +0100

    fixed bugs in loader

--- src/cc/tool/Loader.hx
+++ src/cc/tool/Loader.hx
@@ -5,7 +5,8 @@ import js.html.Image;
 class Loader {
 	@:isVar public var _id(get, set):String;
 	@:isVar public var _loadingArray(get, set):Array<String> = [];
-	public var completeArray:Array<js.html.ImageElement> = [];
+	public var completeArray:Array<js.html.Image> = [];
+	@:isVar public var _isDebug(get, set):Bool = false;
 
 	var _onComplete:Dynamic;
 	var _onCompleteParams:Array<Dynamic>;
@@ -17,30 +18,39 @@ class Loader {
 	var _onErrorParams:Array<Dynamic>;
 	var _loadCounter = 0;
 
-	public function new(id:String) {
-		var _id:Dynamic;
+	public function new(?id:String) {
+		if (id == null) {
+			this._id = '${toString()}_${Date.now().getTime()}';
+		} else {
+			this._id = id;
+		}
 	}
 
 	/**
-	 * var text = Text.create (ctx, 'Matthijs Kamstra aka [mck]').draw();
+	 * import cc.tool.Loader;
 	 *
-	 * @param ctx
-	 * @param text
-	 * @return Text
+	 * var load = Loader.create ().draw();
+	 *
+	 * @return Loader
 	 */
-	static inline public function create(id:String):Loader {
+	static inline public function create(?id:String):Loader {
 		var loader = new Loader(id);
 		return loader;
 	}
 
 	// ____________________________________ properties ____________________________________
 
+	inline public function isDebug(?isDebug:Bool = true):Loader {
+		this._isDebug = isDebug;
+		return this;
+	}
+
 	inline public function add(path:String):Loader {
 		this._loadingArray.push(path);
 		return this;
 	}
 
-	inline public function onComplete(func:Array<js.html.ImageElement>->Void, ?arr:Array<Dynamic>):Loader {
+	inline public function onComplete(func:Array<js.html.Image>->Void, ?arr:Array<Dynamic>):Loader {
 		this._onComplete = func;
 		this._onCompleteParams = arr;
 		return this;
@@ -65,38 +75,56 @@ class Loader {
 	}
 
 	inline public function load():Loader {
-		trace('start loading');
+		if (_isDebug)
+			trace('start loading');
 		loadingHandler();
 		return this;
 	}
 
 	function loadingHandler() {
 		if (_loadCounter >= _loadingArray.length) {
-			trace('${toString()} :: Loading queue is done');
+			if (_isDebug)
+				trace('${toString()} :: Loading queue is done');
+			if (_isDebug)
+				trace(completeArray);
+			if (_isDebug)
+				trace(completeArray.length);
 			if (Reflect.isFunction(_onComplete))
-				Reflect.callMethod(_onComplete, _onComplete, completeArray);
+				Reflect.callMethod(_onComplete, _onComplete, [completeArray]);
 			return;
 		}
 
 		// create the image used
 		var _img = new Image();
+
 		_img.crossOrigin = "Anonymous";
 		_img.src = _loadingArray[_loadCounter];
 		_img.onload = function() {
-			// trace('w: ' + _img.width);
-			// trace('h: ' + _img.height);
+			if (_isDebug)
+				trace('w: ' + _img.width);
+			if (_isDebug)
+				trace('h: ' + _img.height);
+			if (_isDebug)
+				trace(completeArray.length);
 			completeArray.push(_img);
+			if (_isDebug)
+				trace(completeArray);
+			if (_isDebug)
+				trace(completeArray.length);
+
 			if (Reflect.isFunction(_onUpdate))
 				Reflect.callMethod(_onUpdate, _onUpdate, [_img]);
 			_loadCounter++;
 			loadingHandler();
 		}
+
 		_img.onerror = function() {
 			if (Reflect.isFunction(_onError))
 				Reflect.callMethod(_onError, _onError, [_img]);
 			_loadCounter++;
 			loadingHandler();
 		}
+
 		_img.onprogress = function() {
 			if (Reflect.isFunction(_onProgress))
 				Reflect.callMethod(_onProgress, _onProgress, [_img]);
@@ -119,6 +147,14 @@ class Loader {
 
 	function set__loadingArray(value:Array<String>):Array<String> {
 		return _loadingArray = value;
+	}
+
+	function get__isDebug():Bool {
+		return _isDebug;
+	}
+
+	function set__isDebug(value:Bool):Bool {
+		return _isDebug = value;
 	}
 
 	// ____________________________________ misc ____________________________________
