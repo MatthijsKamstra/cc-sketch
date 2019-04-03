7695c0c7b069c6fa43f0220a2d3e042e88528bf4
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-04-03 22:16:54 +0200

    formatting

--- src/cc/model/constants/App.hx
+++ src/cc/model/constants/App.hx
@@ -3,16 +3,12 @@ package cc.model.constants;
 import haxe.macro.Context;
 
 class App {
-
-	public static var NAME : String = "[cc-sketch]";
-	public static var PORT : String = "5000";
-
-
-	public static var BUILD : String = getBuildDate();
+	public static var NAME:String = "[cc-sketch]";
+	public static var PORT:String = "5000";
+	public static var BUILD:String = getBuildDate();
 
 	macro public static function getBuildDate() {
 		var date = Date.now().toString();
 		return Context.makeExpr(date, Context.currentPos());
 	}
-
 }
