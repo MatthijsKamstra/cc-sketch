f2885e774a202e4ece7732cdf0066115aed5c874
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-11-11 22:12:07 +0100

    this fails in Haxe 4 but didn't work great in previous versions... need to investigate

--- src/cc/model/constants/App.hx
+++ src/cc/model/constants/App.hx
@@ -5,10 +5,10 @@ import haxe.macro.Context;
 class App {
 	public static var NAME:String = "[cc-sketch]";
 	public static var PORT:String = "5000";
-	public static var BUILD:String = getBuildDate();
+	// public static var BUILD:String = getBuildDate();
 
-	macro public static function getBuildDate() {
-		var date = Date.now().toString();
-		return Context.makeExpr(date, Context.currentPos());
-	}
+	// macro public static function getBuildDate() {
+	// 	var date = Date.now().toString();
+	// 	return Context.makeExpr(date, Context.currentPos());
+	// }
 }
